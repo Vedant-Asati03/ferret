b423e5bf0a1f375d228de737f01af111074c0632
vedant-asati03 <vedant.asati03@gmail.com>
2026-03-22 00:42:46 +0530

    day02: Printed `Hello World!` to screen, we were given that the buffer is located at `0xb8000`, though this is a very risky implementation, for now it is okay, later we will create a **VGA buffer abstraction**

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,21 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[unsafe(no_mangle)] // we dont want the name of this function to be mangled, so we add this trait
 pub extern "C" fn _start() -> ! {
     // we named the function _start because the linker by-default looks for a function named
     // _start, this function is the custom entry point we defined
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 

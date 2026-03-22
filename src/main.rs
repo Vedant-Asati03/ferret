49add1defd79ff29596fa88f6efd3bcc39d7020b
vedant-asati03 <vedant.asati03@gmail.com>
2026-03-22 19:45:34 +0530

    day03: instead of using the previous error prone implementation, we are now using our abstraction to print to the screen

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,13 @@
 #![no_main] // we will need to implement our own entry point, so we disable all rust-level entry points
 
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 #[unsafe(no_mangle)] // we dont want the name of this function to be mangled, so we add this trait
 pub extern "C" fn _start() -> ! {
     // we named the function _start because the linker by-default looks for a function named
     // _start, this function is the custom entry point we defined
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print();
 
     loop {}
 }
@@ -26,5 +18,3 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-
-
